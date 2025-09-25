d39e9cebe2be5b5dc7922c4a8138d806465d3aaa
Adil Ait Elhoucine <adilaitelhoucine1@gmail.com>
2025-09-25 11:01:18 +0100

    Feat : implement the Other Client Methods in ClientService

--- src/service/ClientService.java
+++ src/service/ClientService.java
@@ -1,6 +1,7 @@
 package service;
 
 import model.Client;
+
 import java.util.*;
 
 public class ClientService {
@@ -9,23 +10,51 @@ public class ClientService {
     public ClientService() {
         this.clients = new HashMap<>();
     }
-    public void AjouterClient(Client c){
-        clients.put(c.getIdClient(),c);
+
+    public void AddClient(Client c) {
+        clients.put(c.getIdClient(), c);
     }
-    public void modifierClient(int idClient, Client client){
-        if(clients.containsKey(idClient)){
-            clients.put(idClient,client);
-        }else{
+
+    public void UpdateClient(int idClient, Client client) {
+        if (clients.containsKey(idClient)) {
+            clients.put(idClient, client);
+        } else {
             throw new NoSuchElementException("aucun utilisateur trouve");
         }
     }
-    public void SupprimerClient(int idClient){
-        if(clients.containsKey(idClient)){
-        clients.remove(idClient);
-        }else{
+
+    public void DeleteClient(int idClient) {
+        if (clients.containsKey(idClient)) {
+            clients.remove(idClient);
+        } else {
             throw new NoSuchElementException("aucun utilisateur trouve");
         }
 
     }
 
+    public Optional<Client> FindClientByID(int idClient) {
+        if (clients.containsKey(idClient)) {
+            return Optional.of(clients.get(idClient));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+
+    public Optional<Client> FindClientByEmail(String email) {
+        for (Map.Entry<Integer, Client> c : clients.entrySet()) {
+            if (c.getValue().getEmail().equals(email)) {
+                return Optional.of(c.getValue());
+            }
+
+        }
+        return Optional.empty();
+    }
+
+    public List<Client> getAllClients(){
+        return  new ArrayList<>(clients.values());
+    }
+    public boolean CheckClient(int ClientID){
+        return clients.containsKey(ClientID);
+    }
 }
